b09674c3d1200ae8d99b33ab8de1ebcb13e119ee
zhuchenshu <1369359429@qq.com>
2017-12-28 22:23:12 +0800

    初始化数据表

---
+++ table/dashiyan/src/main/java/com/shiyan/dashiyan/DashiyanApplication.java
@@ -0,0 +1,12 @@
+package com.shiyan.dashiyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashiyanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashiyanApplication.class, args);
+	}
+}
---
+++ table/dashiyan/src/test/java/com/shiyan/dashiyan/DashiyanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shiyan.dashiyan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DashiyanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
